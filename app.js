73f6eaa5d510dd2feece89bc923f50e1505fee30
Simon Johansson <mail@simon-johansson.com>
2015-09-16 21:38:26 +0200

    Working first prototype

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const faker = require('faker');
 window.jQuery = require('jquery');
 const $ = window.jQuery
 require('bootstrap');
+require('bootstrap-select');
 
 require('./js/tabs');
 require('./js/lorem');
@@ -16,6 +17,10 @@ window.writeToClipboard = (obj) => {
 	ipc.send('new-clipboard', obj);
 }
 
+window.setLocale = (locale) => {
+	ipc.send('set-locale', locale);
+}
+
 $buttons.click(function (e) {
   e.preventDefault();
   const $this = $(this);
@@ -23,4 +28,44 @@ $buttons.click(function (e) {
 	$buttons.removeClass(activeBtnClass);
   $this.addClass(activeBtnClass);
   window.writeToClipboard(method);
+});
+
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) {
+    return typeof args[number] != 'undefined'
+      ? args[number]
+      : match
+    ;
+  });
+};
+
+var avatarTemplate = $("#avatar-template").html();
+
+const populateAvatars = () => {
+	for (var i = 0; i <= 6; i++) {
+		var template = avatarTemplate.format(faker.image.avatar());
+		$("#avatars").prepend(template);
+	};
+}
+populateAvatars();
+
+$("#avatars").on('click', '.load-new', function(event) {
+	event.preventDefault();
+	$("#avatars").find('.img-wrapper').remove();
+	populateAvatars();
+});
+
+$("#avatars").on('click', '.img-wrapper', function(event) {
+	event.preventDefault();
+	const src = $(this).find('img').attr('src');
+	window.writeToClipboard(`fetchImage('${src}')`);
+});
+
+$('.dropdown').on('click', 'li', function(e) {
+	e.preventDefault();
+	const $this = $(this);
+	$('.dropdown li').removeClass('active');
+	$this.addClass('active');
+	window.setLocale($this.data('code'));
 });
--- electron.js
+++ electron.js
@@ -3,6 +3,7 @@ const app = require('app');
 const ipc = require('ipc');
 const clipboard = require('clipboard');
 const globalShortcut = require('global-shortcut');
+const NativeImage = require('native-image');
 const menubar = require('menubar');
 const applescript = require('applescript');
 const faker = require('faker');
@@ -10,9 +11,10 @@ const dataURL = require('./js/dataURL');
 
 var latestCall = null;
 var mb = menubar({
-  "always-on-top": true,
-  "height": 220,
-})
+  // "always-on-top": true,
+  "height": 250,
+  "width": 390,
+});
 
 // report crashes to the Electron project
 require('crash-reporter').start();
@@ -20,20 +22,54 @@ require('crash-reporter').start();
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 
+function sentences(sentenceCount) {
+	if (typeof sentenceCount == 'undefined') { sentenceCount = 3; }
+	var sentences = [];
+	for (; sentenceCount > 0; sentenceCount--) {
+	  sentences.push(faker.lorem.sentence());
+	}
+	return sentences.join(" ");
+}
+
+function paragraph(sentenceCount) {
+	if (typeof sentenceCount == 'undefined') { sentenceCount = 3; }
+	return sentences(sentenceCount + faker.random.number(3));
+};
+
+function paragraphs(paragraphCount) {
+  if (typeof paragraphCount == 'undefined') { paragraphCount = 3; }
+  var paragraphs = [];
+  for (; paragraphCount > 0; paragraphCount--) {
+      paragraphs.push(paragraph());
+  }
+  return paragraphs.join('\n\n');
+}
+
 ipc.on('new-clipboard', function(event, arg) {
 	latestCall = arg;
 	writeToClipboard()
 });
 
-var NativeImage = require('native-image');
-dataURL(faker.image.avatar(), function(data) {
-	var img = NativeImage.createFromDataUrl(data);
-	clipboard.writeImage(img);
-})
+ipc.on('set-locale', function(event, arg) {
+	faker.locale = arg;
+});
+
+function fetchImage(url) {
+	dataURL(url, function(data) {
+		var img = NativeImage.createFromDataUrl(data);
+		clipboard.writeImage(img);
+	});
+}
 
 const writeToClipboard = (method = latestCall) => {
 	if(method) {
-		clipboard.writeText(eval(method));
+		if(method.indexOf('fetchImage') !== -1) {
+			eval(method);
+		} else {
+			var toClip = eval(method);
+			toClip = typeof toClip === 'string' ? toClip : toClip.toDateString();
+			clipboard.writeText(toClip);
+		}
 		setTimeout(() => {
 			mb.hideWindow();
 		}, 200);
